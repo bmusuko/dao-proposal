51fb81b7364b222efb4fe4b14ed2a82a05dc4b1f
Bram Musuko <bram.musuko@gmail.com>
2022-05-08 11:02:37 +0700

    deploy dao contract

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0x71e6D8A2E4847adc852d951895dB9765063577Aa";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
